0c017e7630a39d02101d7db4877a03e1e5eea538
panappi <119737780+panappi@users.noreply.github.com>
2022-12-13 18:12:31 +0900

    FizzBuzz問題

---
+++ main.js
@@ -0,0 +1,25 @@
+/* Fizz buzz */
+for (let num = 0; num <= 30; num++) {
+  const surplus3 = (num) => {
+    return num % 3;
+  };
+  const surplus5 = (num) => {
+    return num % 5;
+  };
+  // 3,5両方の余りが0のときと合致
+  // const surplus15 = (num) => {
+  //   return num % 15;
+  // };
+
+  // console.log(num, surplus3(num), surplus5(num), surplus15(num));
+
+  if (surplus3(num) === 0 && surplus5(num) === 0) {
+    console.log("Fizz Buzz");
+  } else if (surplus3(num) === 0) {
+    console.log("Fizz");
+  } else if (surplus5(num) === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+}
